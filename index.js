42b99ba4529ffed5a09a196a9242cf7172b34cf9
Your Name <sandra909@live.com>
2020-04-30 17:02:08 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var port = process.env.PORT || 5000;
+const tictactoe = require("./tictactoe");
+//var router = express.Router()
+
+// require dotenv to add secret variables
+require('dotenv').config();
+
+// require the discord.js module
+const Discord = require('discord.js');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+const env = process.env;
+
+var game = null;
+
+
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on("message", message => {
+	console.log(message.content);
+		if (message.attachments.size > 0){
+		return message.channel.send(message.attachments);
+	}
+
+	//If the message either doesn't start with the prefix or was sent by a bot, exit early.
+	if (!message.content.startsWith(env.prefix) || message.author.bot) 
+		return;
+
+	// Create an args variable that slices off the prefix entirely and then splits it into an 
+	// array by spaces. Uses regex.
+	const args = message.content.slice(env.prefix.length).split(/ +/);
+
+	// Create a command variable by calling args.shift(), which will take the first element in 
+	// array and return it while also removing it from the original array (so that you don't 
+	// have the command name string inside the args array).
+	const command = args.shift().toLowerCase();
+
+
+	if(command == "henlo"){
+		for(var i = 0; i < parseInt(args[0]); i++){
+			message.channel.send("henlo");
+		}
+	}
+
+	else if(command == "tictac"){
+		createGame(args, message);
+		
+	}
+
+	else if(command == "m"){
+		if(game != null){
+					let status = game.gameStatus();
+		if(status>0){
+			return message.channel.send("Game is over!");
+		} 
+
+		game.makeMove(args[0], args[1]);
+
+		 status = game.gameStatus();
+		if(status>0){
+			return message.channel.send(game.print_board() + "\nPlayer " + status + "wins!");
+		}
+		else if(status == -1){
+			return message.channel.send(game.print_board() + "Game Over: Tie");
+		}
+
+		return message.channel.send(game.print_board() + "\nPlayer " + game.currentTurn() + "'s turn");
+
+		}
+
+	}
+
+});
+
+function createGame(args, message){
+	let size;
+		if(args.length==0){
+			size = 3;
+		}
+		else{
+			size = args[0];
+		}
+		game = new tictactoe(size);
+	return message.channel.send(game.print_board() + "\nPlayer" + game.currentTurn() + "'s turn");
+}
+
+
+
+// login to Discord with your app's token
+client.login(env.TOKEN);
---
+++ tictactoe.js
@@ -0,0 +1,129 @@
+class tictactoe {
+  constructor(size) {
+    this.size = size;
+    this.turn = 1;
+    this.used = 0;
+    this.board = new Array(size);
+
+    for (var i = 0; i < size; i++) {
+	  this.board[i] = new Array(size);
+	}
+
+	for(let row = 0; row<this.size; row++){
+  		for(let col = 0; col < this.size; col++){
+  			this.board[row][col] = 0;
+  		}
+  	}
+  }
+  print_board() {
+  	let board_str = "";
+  	for(let row = -1; row<this.size; row++){
+  		for(let col = -1; col < this.size; col++){
+  			if(row == -1){
+  				if(col > -1)
+  					board_str += col + ":black_large_square:";
+  			}
+  			else if(row > -1 && col > -1){
+	  			if(this.board[row][col]==0){
+	  				board_str += ":white_square_button:"; 
+	  			}
+	  			else if (this.board[row][col]==1){
+	  				board_str += ":regional_indicator_x:"; 
+	  			}
+	  			else if (this.board[row][col]==2){
+	  				board_str += ":regional_indicator_o:"; 
+	  			}
+  			}
+
+  			
+  		}
+  		if (row>-1){
+  			board_str += ":black_large_square:" + row;
+  		}
+  		
+  		board_str += "\n";
+  	}
+    return board_str;
+  }
+
+  currentTurn() {
+  	return this.turn;
+	}
+
+  makeMove(x, y) {
+		if(this.checkPosition(x, y) == 0 ) {
+			this.board[x][y] = this.turn;
+			this.used++;
+			this.updateTurn();
+			return true;
+		}
+		return false;
+	}
+
+
+
+	gameStatus() { // 0 with no winners
+		
+		for(let r =0; r < this.size; r++) {
+			  let row_win;   let col_win; 
+			  let diagLeft_win;   let diagRight_win;
+			row_win = col_win = diagLeft_win = diagRight_win = true;
+			
+			let row_current = this.board[r][0]; 
+			let col_current = this.board[0][r];
+			let diagLeft_current = row_current;
+			let diagRight_current = col_current;
+			
+			for (let c = 0; c < this.size; c++) {
+				
+				row_win = row_win && row_current != 0 && (row_current == this.board[r][c]);
+				col_win = col_win && col_current != 0 && (col_current == this.board[c][r]);
+				diagLeft_win = diagLeft_win && diagLeft_current != 0 && (diagLeft_current == this.board[c][c]);
+				diagRight_win = diagRight_win && diagRight_current != 0 && (diagRight_current == this.board[this.size-c-1][c]);
+				
+				row_current = this.board[r][c];
+				col_current = this.board[c][r];
+				diagLeft_current = this.board[c][c];
+				diagRight_current = this.board[this.size-c-1][c];
+			}
+			if(row_win) {
+				return row_current; // returns player number of winner if won in row
+			}
+			else if(col_win){
+				return col_current; // returns player number of winner if won in col
+			}
+			else if(diagLeft_win) {
+				return diagLeft_current;
+			}
+			else if(diagRight_win) {
+				return diagRight_current;
+			}
+			
+		}
+		
+		if(this.used == this.size * this.size) { // if all the spaces have been filled, thus, a tie
+			return -1;
+		}
+		
+		return 0; // if no winners
+	}
+
+	gameOver() {
+		return gameStatus() != 0;
+	}
+
+  checkPosition(x, y) {
+		return (x > -1 && y > -1 && x  < this.size && y < this.size) ? this.board[x][y] : -1;
+	}
+
+	updateTurn() {
+		this.turn = this.turn == 1? 2: 1; // update the turn on every successful move
+	}
+
+	setTurn(player) {
+		this.turn = player;
+	}
+
+}
+
+module.exports = tictactoe;
